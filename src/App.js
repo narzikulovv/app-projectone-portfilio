4154a9c73063a0aa864453d36c4853b248570bb0
narzikuovv <sardornarzikulov2002@gmail.com>
2021-06-20 17:38:21 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './sass/sardor.scss'
- 
 import './sass/baxtiyor.scss';
 import Navbar from "./Components/Navbar";
 
 
 import Banner from './Components/Banner';
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
 
-<Banner />
-    </div>
-  );
+function App() {
+    return (
+        <div className="App">
+            <Navbar/>
+            <Banner/>
+        </div>
+    );
 }
 
 export default App;
